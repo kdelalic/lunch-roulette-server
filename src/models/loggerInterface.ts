a6d7834567fe41cd886b271ac6071e1d98d3bc86
Karlo Delalic <karlo.delalic1@gmail.com>
2019-02-17 15:24:20 -0500

    Change back to 2 space tab width

--- src/models/loggerInterface.ts
+++ src/models/loggerInterface.ts
@@ -1,7 +1,7 @@
 export interface LoggerInterface {
-    info(message: string, ...supportData: any[]): void;
-    error(message: string, ...supportData: any[]): void;
-    warn(message: string, ...supportData: any[]): void;
-    debug(message: string, ...supportData: any[]): void;
-    trace(message: string, ...supportData: any[]): void;
+  info(message: string, ...supportData: any[]): void;
+  error(message: string, ...supportData: any[]): void;
+  warn(message: string, ...supportData: any[]): void;
+  debug(message: string, ...supportData: any[]): void;
+  trace(message: string, ...supportData: any[]): void;
 }
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,34 +3,34 @@ import { Config } from '../config';
 import { LoggerInterface } from '../models/loggerInterface';
 
 export default class Logger implements LoggerInterface {
-    private logger;
+  private logger;
 
-    constructor() {
-        this.logger = createLogger({
-            format: format.simple(),
-            level: Config.LOGGER_LEVEL,
-            levels: Config.LOGGER_LEVELS,
-            transports: [new transports.Console()]
-        });
-    }
+  constructor() {
+    this.logger = createLogger({
+      format: format.simple(),
+      level: Config.LOGGER_LEVEL,
+      levels: Config.LOGGER_LEVELS,
+      transports: [new transports.Console()]
+    });
+  }
 
-    public error(message: string, ...supportingData: any[]): void {
-        this.logger.error(message, supportingData);
-    }
+  public error(message: string, ...supportingData: any[]): void {
+    this.logger.error(message, supportingData);
+  }
 
-    public warn(message: string, ...supportingData: any[]): void {
-        this.logger.warn(message, supportingData);
-    }
+  public warn(message: string, ...supportingData: any[]): void {
+    this.logger.warn(message, supportingData);
+  }
 
-    public info(message: string, ...supportingData: any[]): void {
-        this.logger.info(message, supportingData);
-    }
+  public info(message: string, ...supportingData: any[]): void {
+    this.logger.info(message, supportingData);
+  }
 
-    public trace(message: string, ...supportingData: any[]): void {
-        this.logger.trace(message, supportingData);
-    }
+  public trace(message: string, ...supportingData: any[]): void {
+    this.logger.trace(message, supportingData);
+  }
 
-    public debug(message: string, ...supportingData: any[]): void {
-        this.logger.debug(message, supportingData);
-    }
+  public debug(message: string, ...supportingData: any[]): void {
+    this.logger.debug(message, supportingData);
+  }
 }
