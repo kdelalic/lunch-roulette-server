2796dfde9d425c7f1aa383616b79e723c1093a72
Karlo Delalic <karlo.delalic@ibm.com>
2019-02-17 15:12:40 -0500

    Change tab size to 4 spaces

--- src/app.ts
+++ src/app.ts
@@ -6,38 +6,38 @@ import { Routes } from './routes';
 import logger from './utils/logger';
 
 class App {
-  public app: express.Application;
-  public routePrv: Routes = new Routes();
-  private log;
+    public app: express.Application;
+    public routePrv: Routes = new Routes();
+    private log;
 
-  constructor() {
-    // Creates new express app
-    this.app = express();
-    // Runs application configuration
-    this.config();
-    // Initializes routes
-    this.routePrv.routes(this.app);
-  }
-
-  // Configures the express application
-  private config(): void {
-    // Initializes logger
-    this.log = new logger();
-
-    // Checks if Yelp api key is present in environment
-    if (!Config.YELP_API_KEY) {
-      this.log.error('YELP_API_KEY is not set. Program stopping.');
-      process.exit(0);
+    constructor() {
+        // Creates new express app
+        this.app = express();
+        // Runs application configuration
+        this.config();
+        // Initializes routes
+        this.routePrv.routes(this.app);
     }
 
-    // Sets CORS options from config file
-    this.app.use(cors(Config.CORS_OPTIONS));
+    // Configures the express application
+    private config(): void {
+        // Initializes logger
+        this.log = new logger();
 
-    // support application/json type post data
-    this.app.use(bodyParser.json());
-    // support application/x-www-form-urlencoded post data
-    this.app.use(bodyParser.urlencoded({ extended: false }));
-  }
+        // Checks if Yelp api key is present in environment
+        if (!Config.YELP_API_KEY) {
+            this.log.error('YELP_API_KEY is not set. Program stopping.');
+            process.exit(0);
+        }
+
+        // Sets CORS options from config file
+        this.app.use(cors(Config.CORS_OPTIONS));
+
+        // support application/json type post data
+        this.app.use(bodyParser.json());
+        // support application/x-www-form-urlencoded post data
+        this.app.use(bodyParser.urlencoded({ extended: false }));
+    }
 }
 
 export default new App().app;
