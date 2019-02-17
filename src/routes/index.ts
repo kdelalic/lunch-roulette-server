19c6da551140dd0c103cabfe354994c531b13680
Karlo Delalic <karlo.delalic1@gmail.com>
2019-02-17 15:12:40 -0500

    Change tab size to 4 spaces

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,65 +3,65 @@ import { Request, Response } from 'express';
 import { Config } from '../config';
 
 export class Routes {
-  private readonly YELP_API_KEY: string = Config.YELP_API_KEY;
+    private readonly YELP_API_KEY: string = Config.YELP_API_KEY;
 
-  public routes(app): void {
-    // Route for REST Yelp restaurants api
-    app.route('/api/restaurants').get((req: Request, res: Response) => {
-      const { latitude, longitude, offset, limit } = req.query;
-      const term = 'restaurants'; // Search term
+    public routes(app): void {
+        // Route for REST Yelp restaurants api
+        app.route('/api/restaurants').get((req: Request, res: Response) => {
+            const { latitude, longitude, offset, limit } = req.query;
+            const term = 'restaurants'; // Search term
 
-      // Log request
-      app.trace(
-        `{latitude: ${latitude}, longitude: ${longitude}, offset: ${offset}, limit: ${limit}}
+            // Log request
+            app.trace(
+                `{latitude: ${latitude}, longitude: ${longitude}, offset: ${offset}, limit: ${limit}}
          from ${req.headers.origin} (${req.headers['user-agent']})`
-      );
+            );
 
-      // Call GET request to yelp api
-      axios({
-        headers: {
-          Authorization: this.YELP_API_KEY
-        },
-        method: 'get',
-        url:
-          'https://api.yelp.com/v3/businesses/search' +
-          `?term=${term}` +
-          `&latitude=${latitude}` +
-          `&longitude=${longitude}` +
-          `&offset=${offset}` +
-          `&limit=${limit}`
-      })
-        .then((result: AxiosResponse) => {
-          // Return response back to our API
-          res.send(result.data.businesses);
-        })
-        .catch((error: AxiosError) => {
-          // Handle error
-          if (error.response) {
-            const { code, description } = error.response.data.error;
-            app.error(`${error.response.status} ${code} ${description}`);
-          } else if (error.request) {
-            app.error(error.request);
-          } else {
-            app.error(error.message);
-          }
+            // Call GET request to yelp api
+            axios({
+                headers: {
+                    Authorization: this.YELP_API_KEY
+                },
+                method: 'get',
+                url:
+                    'https://api.yelp.com/v3/businesses/search' +
+                    `?term=${term}` +
+                    `&latitude=${latitude}` +
+                    `&longitude=${longitude}` +
+                    `&offset=${offset}` +
+                    `&limit=${limit}`
+            })
+                .then((result: AxiosResponse) => {
+                    // Return response back to our API
+                    res.send(result.data.businesses);
+                })
+                .catch((error: AxiosError) => {
+                    // Handle error
+                    if (error.response) {
+                        const { code, description } = error.response.data.error;
+                        app.error(`${error.response.status} ${code} ${description}`);
+                    } else if (error.request) {
+                        app.error(error.request);
+                    } else {
+                        app.error(error.message);
+                    }
+                });
         });
-    });
 
-    // Route for GraphQL Yelp restaurants api
-    app.route('/api/graphql/restaurants').get((req: Request, res: Response) => {
-      const { latitude, longitude, offset, limit } = req.query;
-      const term = 'restaurants';
+        // Route for GraphQL Yelp restaurants api
+        app.route('/api/graphql/restaurants').get((req: Request, res: Response) => {
+            const { latitude, longitude, offset, limit } = req.query;
+            const term = 'restaurants';
 
-      // Log request
-      app.trace(
-        `{latitude: ${latitude}, longitude: ${longitude}, offset: ${offset}, limit: ${limit}}
+            // Log request
+            app.trace(
+                `{latitude: ${latitude}, longitude: ${longitude}, offset: ${offset}, limit: ${limit}}
          from ${req.headers.origin} (${req.headers['user-agent']})`
-      );
+            );
 
-      // Call GET request to Yelp GraphQL API
-      axios({
-        data: `{
+            // Call GET request to Yelp GraphQL API
+            axios({
+                data: `{
           search(
             term: ${term},
             latitude: ${latitude},
@@ -80,27 +80,27 @@ export class Routes {
             }
           }
         }`,
-        headers: {
-          Authorization: this.YELP_API_KEY
-        },
-        method: 'post',
-        url: 'https://api.yelp.com/v3/graphql'
-      })
-        .then((result: AxiosResponse) => {
-          // Return response back to our API
-          res.send(result.data.data.search.business);
-        })
-        .catch((error: AxiosError) => {
-          // Handle error
-          if (error.response) {
-            const { code, description } = error.response.data.error;
-            app.error(`${error.response.status} ${code} ${description}`);
-          } else if (error.request) {
-            app.error(error.request);
-          } else {
-            app.error(error.message);
-          }
+                headers: {
+                    Authorization: this.YELP_API_KEY
+                },
+                method: 'post',
+                url: 'https://api.yelp.com/v3/graphql'
+            })
+                .then((result: AxiosResponse) => {
+                    // Return response back to our API
+                    res.send(result.data.data.search.business);
+                })
+                .catch((error: AxiosError) => {
+                    // Handle error
+                    if (error.response) {
+                        const { code, description } = error.response.data.error;
+                        app.error(`${error.response.status} ${code} ${description}`);
+                    } else if (error.request) {
+                        app.error(error.request);
+                    } else {
+                        app.error(error.message);
+                    }
+                });
         });
-    });
-  }
+    }
 }
