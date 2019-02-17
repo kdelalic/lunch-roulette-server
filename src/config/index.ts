aa044f8cc58f4404ff89eccb08f0cf481125ddfc
Karlo Delalic <karlo.delalic@ibm.com>
2019-02-17 00:19:46 -0500

    Readd cors

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,11 +4,15 @@ export class Config {
 
   public static DEVELOPMENT_MODE: boolean = process.env.NODE_ENV !== 'production';
 
-  public static SERVER_ORIGIN: string = process.env.SERVER_ORIGIN || 'http://localhost:3000';
-
   public static LOGGER_LEVEL: string = process.env.LOGGER_LEVEL || 'info';
 
   public static LOGGER_LEVELS = { error: 0, warn: 1, info: 2, trace: 3, debug: 4 };
 
   public static PORT = process.env.PORT || 5000;
+
+  public static CORS_OPTIONS = {
+    corsOptions:
+      process.env.NODE_ENV === 'production' ? process.env.SERVER_ORIGIN : 'http://localhost:3000',
+    optionsSuccessStatus: 200
+  };
 }
